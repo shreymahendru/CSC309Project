37a4ea5c4a5735fe76a279c23f5cc3b855aa6c1f
shreymahendru <shreymahendru@gmail.com>
2016-04-06 02:36:51 -0400

    admin button  on navbar

--- PaperGrader/app/controllers/NavBarController.js
+++ PaperGrader/app/controllers/NavBarController.js
@@ -10,10 +10,10 @@
          $http.get(query).success(function(response){
              console.log(response);
              $scope.user = response.local;
-            //  console.log($scope.user.admin);
-            //  if($scope.user.admin) {
-            //    $rootScope.admin = true;
-            //  }
+              console.log($scope.user.admin);
+              if($scope.user.admin) {
+                $scope.admin = true;
+              }
          }).error(function(error){
              console.log(error);
          })
